75bbcb0b29398a253dbd8b21c9482a331550af55
Anton Österberg <anton.osterberg@gmail.com>
2016-12-09 00:53:00 +0100

    We are off to a start...

--- src/com/mantono/syno/Word.java
+++ src/com/mantono/syno/Word.java
@@ -7,30 +7,41 @@ public class Word
 {
 	private final String word;
 	private final Map<String, Double> weights;
-	
+
 	public Word(final String word)
 	{
 		this.word = word.toLowerCase();
 		this.weights = new HashMap<String, Double>();
 	}
-	
-	public void add(final String context)
+
+	public void add(String context)
 	{
+		context = context.toLowerCase();
 		if(!context.contains(word))
 			throw new IllegalArgumentException("Missing the word " + word + " in context.");
 		String[] words = context.split("\\s+");
 		final int wordIndex = findIndex(words);
-		
-		int index = wordIndex-2;
-		if(index >= 0)
-			weights.put(words[index], 0.5);
-		if(++index >= 0)
-			weights.put(words[index], 1.0);
-		index++;
-		if(++index < words.length)
-			weights.put(words[index], 1.0);
-		if(++index < words.length)
-			weights.put(words[index], 0.5);
+
+		for(int distance = 1; distance < 3; distance++)
+		{
+			final double weight = Math.pow(2, 1 - distance);
+			final int left = wordIndex - distance;
+			final int right = wordIndex + distance;
+			
+			if(left >= 0)
+				addWeight(words[left], weight);
+			
+			if(right < words.length)
+				addWeight(words[right], weight);
+		}
+	}
+
+	private void addWeight(String string, double additionalWeight)
+	{
+		double currentWeight = 0;
+		if(weights.containsKey(string))
+			currentWeight = weights.get(string);
+		weights.put(string, currentWeight+additionalWeight);	
 	}
 
 	private int findIndex(String[] words)
@@ -39,5 +50,11 @@ public class Word
 			if(words[i].equals(word))
 				return i;
 		return -1;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return word.toString() + " " + weights.toString();
 	}
 }
